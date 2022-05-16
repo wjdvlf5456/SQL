86a60045ec319058cb0f309b76b39d887db627a3
최정필 <wjdvlf1407@gmail.com>
2022-05-16 10:13:02 +0900

    1-10

--- practice/practice01.sql
+++ practice/practice01.sql
@@ -108,8 +108,11 @@ ORDER BY country_name ASC;
 문제10.
 입사일이 03/12/31 일 이전 입사한 직원의 이름, 월급, 전화 번호, 입사일을 출력하세요 전화번호는 545-343-3433 과 같은 형태로 출력하시오.
  */
-
-
-
-
-
+SELECT 
+			first_name || ' ' || last_name "이름",
+			salary "월급",
+			REPLACE (phone_number,'.','-') "전화번호",
+			TO char(hire_date, 'YY/MM/DD') "입사일"
+FROM 
+employees
+WHERE hire_date <= '03/12/31';
